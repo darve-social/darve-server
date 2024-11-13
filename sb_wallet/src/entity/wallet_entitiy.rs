58ab1ed7c3be687f2237df4f26f4b7dc16663bfb
matjazonline <matjazonline@gmail.com>
2024-11-14 00:30:55 +0100

    wallet

---
+++ sb_wallet/src/entity/wallet_entitiy.rs
@@ -0,0 +1,66 @@
+use sb_middleware::db;
+use sb_middleware::utils::db_utils::{get_entity, get_entity_list, with_not_found_err, IdentIdName};
+use sb_middleware::{
+    ctx::Ctx,
+    error::{AppError, CtxError, CtxResult},
+};
+use sb_user_auth::entity::access_rule_entity::AccessRule;
+use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
+use surrealdb::opt::PatchOp;
+use surrealdb::sql::Thing;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Wallet {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<Thing>,
+    pub user: Thing,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub r_created: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub r_updated: Option<String>,
+}
+
+pub struct WalletDbService<'a> {
+    pub db: &'a db::Db,
+    pub ctx: &'a Ctx,
+}
+
+pub const TABLE_NAME: &str = "wallet";
+const USER_TABLE: &str = sb_user_auth::entity::local_user_entity::TABLE_NAME;
+
+impl<'a> WalletDbService<'a> {
+    pub async fn mutate_db(&self) -> Result<(), AppError> {
+        let sql = format!("
+    DEFINE TABLE {TABLE_NAME} SCHEMAFULL;
+    DEFINE FIELD user ON TABLE {TABLE_NAME} TYPE record<{USER_TABLE}> VALUE $before OR <record>{USER_TABLE}:$this.id();
+    DEFINE FIELD r_created ON TABLE {TABLE_NAME} TYPE option<datetime> DEFAULT time::now() VALUE $before OR time::now();
+    DEFINE FIELD r_updated ON TABLE {TABLE_NAME} TYPE option<datetime> DEFAULT time::now() VALUE time::now();
+    ");
+        let mutation = self.db
+            .query(sql)
+            .await?;
+
+        &mutation.check().expect("should mutate wallet");
+
+        Ok(())
+    }
+
+    pub async fn create(&self, record: Wallet) -> CtxResult<Wallet> {
+        let res = self.db
+            .create(TABLE_NAME)
+            .content(record)
+            .await
+            .map_err(CtxError::from(self.ctx))
+            .map(|v: Option<Wallet>| v.unwrap());
+
+        // let things: Vec<Domain> = self.db.select(TABLE_NAME).await.ok().unwrap();
+        // dbg!(things);
+        res
+    }
+
+    pub async fn get(&self, ident: IdentIdName) -> CtxResult<Wallet> {
+        let opt = get_entity::<Wallet>(&self.db, TABLE_NAME.to_string(), &ident).await?;
+        with_not_found_err(opt, self.ctx, &ident.to_string().as_str())
+    }
+}
---
+++ sb_wallet/src/routes/wallet_routes.rs
@@ -0,0 +1,223 @@
+use crate::entity::task_request_entitiy::{TaskRequest, TaskRequestDbService, TaskStatus, UserTaskRole};
+use axum::body::Body;
+use axum::extract::{DefaultBodyLimit, Path, Request, State};
+use axum::http::uri::PathAndQuery;
+use axum::http::{Response, Uri};
+use axum::response::Html;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
+use sb_middleware::ctx::Ctx;
+use sb_middleware::error::AppError::AuthorizationFail;
+use sb_middleware::error::{AppError, CtxError, CtxResult};
+use sb_middleware::mw_ctx::CtxState;
+use sb_middleware::utils::db_utils::IdentIdName;
+use sb_middleware::utils::extractor_utils::JsonOrFormValidated;
+use sb_middleware::utils::request_utils::CreatedResponse;
+use sb_middleware::utils::string_utils::get_string_thing;
+use sb_user_auth::entity::local_user_entity::LocalUserDbService;
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use std::fmt::{Display, Formatter};
+use std::path::Path as FPath;
+use surrealdb::sql::Thing;
+use tempfile::NamedTempFile;
+use tower::util::ServiceExt;
+use tower_http::services::fs::ServeFileSystemResponseBody;
+use validator::Validate;
+
+
+pub fn routes(state: CtxState) -> Router {
+
+    Router::new()
+        .route("/api/wallet", post(create_entity))
+        .with_state(state)
+}
+
+#[derive(Deserialize, Serialize, Validate)]
+pub struct TaskRequestInput {
+    #[validate(length(min = 5, message = "Min 5 characters"))]
+    pub content: String,
+    #[validate(length(min = 5, message = "Min 5 characters"))]
+    pub to_user: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub post_id: Option<String>,
+    pub offer_amount: u64,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct AcceptTaskRequestInput {
+    pub accept: bool,
+}
+
+#[derive(Validate, TryFromMultipart)]
+pub struct DeliverTaskRequestInput {
+    #[form_data(limit = "30MiB")]
+    pub file_1: FieldData<NamedTempFile>,
+}
+
+async fn post_requests_received(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+    Path(post_id): Path<String>,
+) -> CtxResult<String> {
+    let to_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+    let post_id = Thing::try_from(post_id).map_err(|e| ctx.to_ctx_error(AppError::Generic { description: "error into post_id Thing".to_string() }))?;
+
+    let list = TaskRequestDbService { db: &_db, ctx: &ctx }.user_post_list(UserTaskRole::ToUser, to_user, post_id).await?;
+    serde_json::to_string(&list).map_err(|e| ctx.to_ctx_error(e.into()))
+}
+
+async fn post_requests_given(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+    Path(post_id): Path<String>,
+) -> CtxResult<String> {
+    let from_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+    let post_id = Thing::try_from(post_id).map_err(|e| ctx.to_ctx_error(AppError::Generic { description: "error into post_id Thing".to_string() }))?;
+
+    let list = TaskRequestDbService { db: &_db, ctx: &ctx }.user_post_list(UserTaskRole::FromUser, from_user, post_id).await?;
+    serde_json::to_string(&list).map_err(|e| ctx.to_ctx_error(e.into()))
+}
+
+async fn user_task_requests_accepted(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+) -> CtxResult<String> {
+    let from_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+
+    let list = TaskRequestDbService { db: &_db, ctx: &ctx }.user_status_list(UserTaskRole::FromUser, from_user, TaskStatus::Accepted).await?;
+    serde_json::to_string(&list).map_err(|e| ctx.to_ctx_error(e.into()))
+}
+
+async fn user_task_requests_delivered(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+) -> CtxResult<String> {
+    let from_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+
+    let list = TaskRequestDbService { db: &_db, ctx: &ctx }.user_status_list(UserTaskRole::FromUser, from_user, TaskStatus::Delivered).await?;
+    serde_json::to_string(&list).map_err(|e| ctx.to_ctx_error(e.into()))
+}
+
+async fn create_entity(State(CtxState { _db, .. }): State<CtxState>,
+                       ctx: Ctx,
+                       JsonOrFormValidated(t_request_input): JsonOrFormValidated<TaskRequestInput>,
+) -> CtxResult<Html<String>> {
+    let from_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+
+    let to_user = if t_request_input.to_user.len() > 0 {
+        Thing::try_from(t_request_input.to_user).map_err(|e| AppError::Generic { description: "to_user error into id Thing".to_string() })?
+    } else {
+        return Err(ctx.to_ctx_error(AppError::Generic { description: "to_user must have value".to_string() }));
+    };
+
+    let content = if t_request_input.content.len() > 0 {
+        t_request_input.content
+    } else {
+        return Err(ctx.to_ctx_error(AppError::Generic { description: "content must have value".to_string() }));
+    };
+
+    let offer_amount = if t_request_input.offer_amount > 0 {
+        t_request_input.offer_amount
+    } else {
+        return Err(ctx.to_ctx_error(AppError::Generic { description: "offer_amount must have value greater than 0".to_string() }));
+    };
+
+    let post_id = t_request_input.post_id.unwrap_or("".to_string());
+    let post = if post_id.len() > 0 {
+        Some(Thing::try_from(post_id).map_err(|e| AppError::Generic { description: "post_id error into id Thing".to_string() })?)
+    } else {
+        None
+    };
+
+    let t_request = TaskRequestDbService { db: &_db, ctx: &ctx }.create(TaskRequest { id: None, from_user, to_user, request_post: post, request_txt: content, offer_amount, status: TaskStatus::Requested.to_string(), deliverables: None, deliverables_post: None, r_created: None, r_updated: None }).await?;
+
+    ctx.to_htmx_or_json_res(CreatedResponse { id: t_request.id.unwrap().to_raw(), uri: None, success: true })
+}
+
+async fn serve_task_deliverable_file(State(CtxState { _db, uploads_serve_dir, .. }): State<CtxState>,
+                                     ctx: Ctx,
+                                     Path(path): Path<String>,
+) -> Result<Response<ServeFileSystemResponseBody>, CtxError> {
+    let user = get_string_thing(ctx.user_id()?)?;
+
+    let task_file = TaskDeliverableFileName::try_from(path.clone())?;
+    let task = TaskRequestDbService { db: &_db, ctx: &ctx }.get(IdentIdName::Id(task_file.task_id.to_raw())).await?;
+    if task.from_user != user {
+        return Err(ctx.to_ctx_error(AuthorizationFail { required: "Not authorised".to_string() }));
+    }
+
+    let uri = Uri::from(PathAndQuery::try_from(path).unwrap());
+    let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
+    let res = uploads_serve_dir.oneshot(req).await;
+        res.map_err(|e| ctx.to_ctx_error(AppError::Generic { description: "Error getting file".to_string() }))
+}
+
+async fn accept_task_request(State(CtxState { _db, .. }): State<CtxState>,
+                             ctx: Ctx,
+                             Path(task_id): Path<String>,
+                             Json(t_request_input): Json<AcceptTaskRequestInput>,
+) -> CtxResult<Html<String>> {
+    let to_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+    let task_id = get_string_thing(task_id)?;
+    let status = match t_request_input.accept {
+        true => TaskStatus::Accepted,
+        false => TaskStatus::Rejected
+    };
+    TaskRequestDbService { db: &_db, ctx: &ctx }.update_status_received_by_user(to_user, task_id.clone(), status, None).await?;
+
+    ctx.to_htmx_or_json_res(CreatedResponse { id: task_id.to_raw(), uri: None, success: true })
+}
+
+async fn deliver_task_request(State(CtxState { _db, uploads_dir, .. }): State<CtxState>,
+                              ctx: Ctx,
+                              Path(task_id): Path<String>,
+                              TypedMultipart(t_request_input): TypedMultipart<DeliverTaskRequestInput>,
+) -> CtxResult<Html<String>> {
+    let to_user = LocalUserDbService { db: &_db, ctx: &ctx }.get_ctx_user_thing().await?;
+    let task_id = get_string_thing(task_id)?;
+
+    let file_name = t_request_input.file_1.metadata.file_name.unwrap();
+    let ext = file_name.split(".").last().ok_or(AppError::Generic { description: "File has no extension".to_string() })?;
+
+    let file_name: String = TaskDeliverableFileName { task_id: task_id.clone(), file_nr: 1, ext: ext.to_string() }.to_string();
+    let path = FPath::new(&uploads_dir).join(file_name.clone());
+    t_request_input.file_1.contents.persist(path.clone())
+        .map_err(|e| ctx.to_ctx_error(AppError::Generic { description: "Upload failed".to_string() }))?;
+    let file_uri = format!("{DELIVERIES_URL_BASE}/{file_name}");
+
+    TaskRequestDbService { db: &_db, ctx: &ctx }.update_status_received_by_user(to_user, task_id.clone(), TaskStatus::Delivered, Some(vec![file_uri])).await?;
+
+    ctx.to_htmx_or_json_res(CreatedResponse { id: task_id.to_raw(), uri: None, success: true })
+}
+
+struct TaskDeliverableFileName {
+    task_id: Thing,
+    ext: String,
+    file_nr: i8,
+}
+
+impl Display for TaskDeliverableFileName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(format!("tid_{}-file_{}.{}", self.task_id.id.to_raw(), self.file_nr, self.ext).as_str())
+    }
+}
+
+impl TryFrom<String> for TaskDeliverableFileName {
+    type Error = AppError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let error = AppError::Generic { description: "Can not parse task file".to_string() };
+        let tid_fname = value.split_once("-").ok_or(error.clone())?;
+        let tid = tid_fname.0.split_once("_").ok_or(error.clone())?.1;
+        let task_id = Thing::from((crate::entity::task_request_entitiy::TABLE_NAME, tid));
+
+        let fnr_ext = tid_fname.1.split_once(".").ok_or(error.clone())?;
+        let fnr = fnr_ext.0.split_once("_").ok_or(error.clone())?;
+        let file_nr: i8 = from_str(fnr.1).map_err(|e| error.clone())?;
+        let ext = fnr_ext.1.to_string();
+
+        Ok(TaskDeliverableFileName { ext, file_nr, task_id })
+    }
+}
