56639bd2d206057ff9944ca67ed7171ec4b2e967
Anukul Pandey <anukul.030601@gmail.com>
2024-12-01 17:47:49 +0530

    feat: bakugo image url dummy

--- server_main/src/main.rs
+++ server_main/src/main.rs
@@ -75,7 +75,7 @@ async fn main() -> AppResult<()> {
         let email = "dynamite@myheroacademia.io".to_string();
         let bio = "💥 Hero-in-training with explosive ambition to be #1! 💣".to_string();
         let full_name = "Katsuki Bakugo".to_string();
-        let image_uri = "https://images.unsplash.com/photo-1611272585212-5a3b87b63294?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D".to_string();
+        let image_uri = "https://qph.cf2.quoracdn.net/main-qimg-64a32df103bc8fb7b2fc495553a5fc0a-lq".to_string();
         create_dev_env(&ctx_state.clone(), username.clone(), password.clone(),Some(email.clone()),
         Some(bio.clone()),
         Some(image_uri.clone()),
