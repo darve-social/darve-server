d96b260040d58a408734d8214a0031cfd47d6335
Anukul Pandey <anukul.030601@gmail.com>
2024-12-03 14:30:59 +0530

    feat: user details route

--- sb_community/src/routes/profile_routes.rs
+++ sb_community/src/routes/profile_routes.rs
@@ -6,9 +6,11 @@ use askama_axum::axum_core::extract::DefaultBodyLimit;
 use askama_axum::axum_core::response::IntoResponse;
 use askama_axum::Template;
 use axum::extract::{Path, State};
+use axum::http::StatusCode;
 use axum::response::Html;
+use axum::response::Response;
 use axum::routing::{get, post};
-use axum::Router;
+use axum::{Json, Router};
 use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
 use futures::TryFutureExt;
 use serde::{Deserialize, Serialize};
@@ -25,7 +27,9 @@ use sb_middleware::ctx::Ctx;
 use sb_middleware::db::Db;
 use sb_middleware::error::{AppError, CtxResult};
 use sb_middleware::mw_ctx::CtxState;
-use sb_middleware::utils::db_utils::{get_entities_by_id, record_exists, IdentIdName, ViewFieldSelector};
+use sb_middleware::utils::db_utils::{
+    get_entities_by_id, record_exists, IdentIdName, ViewFieldSelector,
+};
 use sb_middleware::utils::extractor_utils::DiscussionParams;
 use sb_middleware::utils::request_utils::CreatedResponse;
 use sb_middleware::utils::string_utils::get_string_thing;
@@ -37,11 +41,15 @@ use sb_user_auth::utils::template_utils::ProfileFormPage;
 pub fn routes(state: CtxState) -> Router {
     Router::new()
         .route("/u/:username", get(display_profile))
+        .route("/api/u/:username", get(get_user_by_username))
         .route("/u/following/posts", get(get_following_posts))
         .route("/accounts/edit", get(profile_form))
         .route("/api/accounts/edit", post(profile_save))
         .route("/api/user_chat/list", get(get_chats))
-        .route("/api/user_chat/with/:other_user_id", get(get_create_chat_discussion))
+        .route(
+            "/api/user_chat/with/:other_user_id",
+            get(get_create_chat_discussion),
+        )
         .layer(DefaultBodyLimit::max(1024 * 1024 * 1))
         .with_state(state)
 }
@@ -53,7 +61,6 @@ pub struct ProfileSettingsForm {
     pub full_name: String,
     pub email: String,
     pub image_url: String,
-
 }
 
 #[derive(Validate, TryFromMultipart)]
@@ -141,13 +148,59 @@ pub struct ProfileChat {
     pub discussion: Discussion,
 }
 
-async fn profile_form(
-    State(ctx_state): State<CtxState>,
-    ctx: Ctx,
-) -> CtxResult<ProfileFormPage> {
-    let user = LocalUserDbService { db: &ctx_state._db, ctx: &ctx }.get_ctx_user().await?;
+#[derive(Debug, Serialize)]
+struct UserDetails {
+    id: String,
+    username: String,
+    full_name: Option<String>,
+    image_uri: Option<String>,
+    bio: Option<String>,
+    email: Option<String>,
+    followers_nr: i64,
+    following_nr: i64,
+}
 
-    Ok(ProfileFormPage::new(Box::new(ProfileSettingsForm { username: user.username, full_name: "".to_string(), email: user.email.unwrap_or_default(), image_url: user.image_uri.unwrap_or_default() }), None, None))
+pub async fn get_user_by_username(
+    State(CtxState { _db, key_enc, .. }): State<CtxState>,
+    ctx: Ctx,
+    Path(username): Path<String>,
+) -> CtxResult<Response> {
+    println!("->> {:<12} - get_user_by_username", "HANDLER");
+    let local_user_db_service = LocalUserDbService {
+        ctx: &ctx,
+        db: &_db,
+    };
+
+    let user = local_user_db_service
+        .get_user_by_username(&username)
+        .await?;
+
+        let mut profile_view = local_user_db_service
+        .get_view::<ProfileView>(IdentIdName::ColumnIdent { column: "username".to_string(), val: username.clone(), rec: false }).await?;
+    
+        let mut res = (StatusCode::OK, Json(UserDetails { id: user.id.map_or("".to_string(), |thing| thing.to_string()), username:username.clone(),email:user.email.clone(),full_name:user.full_name.clone(),bio:user.bio.clone(),image_uri:user.image_uri.clone(),followers_nr:profile_view.followers_nr,following_nr:profile_view.following_nr })).into_response();
+
+        Ok(res)
+}
+
+async fn profile_form(State(ctx_state): State<CtxState>, ctx: Ctx) -> CtxResult<ProfileFormPage> {
+    let user = LocalUserDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    }
+    .get_ctx_user()
+    .await?;
+
+    Ok(ProfileFormPage::new(
+        Box::new(ProfileSettingsForm {
+            username: user.username,
+            full_name: "".to_string(),
+            email: user.email.unwrap_or_default(),
+            image_url: user.image_uri.unwrap_or_default(),
+        }),
+        None,
+        None,
+    ))
 }
 
 async fn profile_save(
@@ -155,23 +208,44 @@ async fn profile_save(
     ctx: Ctx,
     TypedMultipart(body_value): TypedMultipart<ProfileSettingsFormInput>,
 ) -> CtxResult<Html<String>> {
-    let mut user = LocalUserDbService { db: &ctx_state._db, ctx: &ctx }.get_ctx_user().await?;
+    let mut user = LocalUserDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    }
+    .get_ctx_user()
+    .await?;
 
-    let local_user_db_service = LocalUserDbService { db: &ctx_state._db, ctx: &ctx };
-    body_value.validate().map_err(|e1| ctx.to_ctx_error(AppError::Generic { description: e1.to_string() }))?;
+    let local_user_db_service = LocalUserDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    };
+    body_value.validate().map_err(|e1| {
+        ctx.to_ctx_error(AppError::Generic {
+            description: e1.to_string(),
+        })
+    })?;
     user.email = if body_value.email.trim().len() > 0 {
         Some(body_value.email.trim().to_string())
-    } else { user.email };
+    } else {
+        user.email
+    };
 
     user.username = if body_value.username.trim().len() > 0 {
         body_value.username.trim().to_string()
-    } else { user.username };
+    } else {
+        user.username
+    };
 
     if let Some(image_file) = body_value.image_url {
         let file_name = image_file.metadata.file_name.unwrap();
-        let ext = file_name.split(".").last().ok_or(AppError::Generic { description: "File has no extension".to_string() })?;
+        let ext = file_name.split(".").last().ok_or(AppError::Generic {
+            description: "File has no extension".to_string(),
+        })?;
 
-        let file_name = format!("uid_{}-profile_image.{ext}", user.id.clone().unwrap().id.to_raw());
+        let file_name = format!(
+            "uid_{}-profile_image.{ext}",
+            user.id.clone().unwrap().id.to_raw()
+        );
         let path = FPath::new(&ctx_state.uploads_dir).join(file_name.as_str());
         let saved = image_file.contents.persist(path.clone());
         if saved.is_ok() {
@@ -180,7 +254,11 @@ async fn profile_save(
     }
 
     let user = local_user_db_service.update(user).await?;
-    ctx.to_htmx_or_json_res(CreatedResponse { id: user.id.unwrap().to_raw(), uri: Some(user.username), success: true })
+    ctx.to_htmx_or_json_res(CreatedResponse {
+        id: user.id.unwrap().to_raw(),
+        uri: Some(user.username),
+        success: true,
+    })
 }
 
 async fn display_profile(
@@ -189,25 +267,50 @@ async fn display_profile(
     Path(username): Path<String>,
     q_params: DiscussionParams,
 ) -> CtxResult<Html<String>> {
-    let local_user_db_service = LocalUserDbService { db: &ctx_state._db, ctx: &ctx };
+    let local_user_db_service = LocalUserDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    };
     let mut profile_view = local_user_db_service
-        .get_view::<ProfileView>(IdentIdName::ColumnIdent { column: "username".to_string(), val: username, rec: false }).await?;
-    let profile_comm = get_profile_community(&ctx_state._db, &ctx, profile_view.user_id.clone()).await?;
+        .get_view::<ProfileView>(IdentIdName::ColumnIdent {
+            column: "username".to_string(),
+            val: username,
+            rec: false,
+        })
+        .await?;
+    let profile_comm =
+        get_profile_community(&ctx_state._db, &ctx, profile_view.user_id.clone()).await?;
     profile_view.community = profile_comm.id;
     profile_view.profile_discussion = profile_comm.profile_discussion;
 
     let disc_id = profile_view.profile_discussion.clone().unwrap();
-    let mut dis_view = DiscussionDbService { db: &ctx_state._db, ctx: &ctx }.get_view::<ProfileDiscussionView>(IdentIdName::Id(disc_id.clone())).await?;
+    let mut dis_view = DiscussionDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    }
+    .get_view::<ProfileDiscussionView>(IdentIdName::Id(disc_id.clone()))
+    .await?;
 
-    let discussion_posts = PostDbService { db: &ctx_state._db, ctx: &ctx }
-        .get_by_discussion_desc_view::<ProfilePostView>(disc_id.clone(), q_params.clone()).await?;
+    let discussion_posts = PostDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    }
+    .get_by_discussion_desc_view::<ProfilePostView>(disc_id.clone(), q_params.clone())
+    .await?;
     dis_view.posts = discussion_posts;
 
     profile_view.profile_discussion_view = Some(dis_view);
-    let follow_db_service = FollowDbService { db: &ctx_state._db, ctx: &ctx };
+    let follow_db_service = FollowDbService {
+        db: &ctx_state._db,
+        ctx: &ctx,
+    };
     // TODO cache user follow numbers
-    profile_view.following_nr = follow_db_service.user_following_number(profile_view.user_id.clone()).await?;
-    profile_view.followers_nr =follow_db_service.user_followers_number(profile_view.user_id.clone()).await?;
+    profile_view.following_nr = follow_db_service
+        .user_following_number(profile_view.user_id.clone())
+        .await?;
+    profile_view.followers_nr = follow_db_service
+        .user_followers_number(profile_view.user_id.clone())
+        .await?;
 
     ctx.to_htmx_or_json_res(ProfilePage {
         theme_name: "emerald".to_string(),
@@ -236,73 +339,141 @@ async fn get_profile_community(db: &Db, ctx: &Ctx, user_id: Thing) -> CtxResult<
 }
 
 // posts user is following
-async fn get_following_posts(State(CtxState{_db,..}): State<CtxState>,
-                           ctx: Ctx,
-                           q_params: DiscussionParams) -> CtxResult<DiscussionView> {
-    let local_user_db_service = LocalUserDbService { db: &_db, ctx: &ctx };
+async fn get_following_posts(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+    q_params: DiscussionParams,
+) -> CtxResult<DiscussionView> {
+    let local_user_db_service = LocalUserDbService {
+        db: &_db,
+        ctx: &ctx,
+    };
     let user_id = local_user_db_service.get_ctx_user_thing().await?;
-    let following_posts_disc = CommunityDbService{ db: &_db, ctx: &ctx }.get_profile_following_posts_discussion(user_id).await?;
+    let following_posts_disc = CommunityDbService {
+        db: &_db,
+        ctx: &ctx,
+    }
+    .get_profile_following_posts_discussion(user_id)
+    .await?;
     get_discussion_view(&_db, &ctx, following_posts_disc, q_params).await
 }
 
 // user chat discussions
-async fn get_chats(State(CtxState { _db, .. }): State<CtxState>,
-                   ctx: Ctx,
+async fn get_chats(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
 ) -> CtxResult<Html<String>> {
     println!("->> {:<12} - get_chats", "HANDLER");
-    let local_user_db_service = LocalUserDbService { db: &_db, ctx: &ctx };
+    let local_user_db_service = LocalUserDbService {
+        db: &_db,
+        ctx: &ctx,
+    };
     let user_id = local_user_db_service.get_ctx_user_thing().await?;
     let comm = get_profile_community(&_db, &ctx, user_id.clone()).await?;
     let discussion_ids = comm.profile_chats.unwrap_or(vec![]);
     let discussions = get_entities_by_id::<Discussion>(&_db, discussion_ids).await?;
-    ctx.to_htmx_or_json_res(ProfileChatList { user_id, discussions })
+    ctx.to_htmx_or_json_res(ProfileChatList {
+        user_id,
+        discussions,
+    })
 }
 
-async fn get_create_chat_discussion(State(CtxState { _db, .. }): State<CtxState>,
-                                    ctx: Ctx,
-                                    Path(other_user_id): Path<String>,
+async fn get_create_chat_discussion(
+    State(CtxState { _db, .. }): State<CtxState>,
+    ctx: Ctx,
+    Path(other_user_id): Path<String>,
 ) -> CtxResult<Html<String>> {
     println!("->> {:<12} - get get_chat_discussion", "HANDLER");
-    let local_user_db_service = LocalUserDbService { db: &_db, ctx: &ctx };
+    let local_user_db_service = LocalUserDbService {
+        db: &_db,
+        ctx: &ctx,
+    };
     let user_id = local_user_db_service.get_ctx_user_thing().await?;
     let other_user_id = get_string_thing(other_user_id)?;
     // TODO limit nr of requests or count them to distinguish bots for user ids
-    local_user_db_service.exists(IdentIdName::Id(other_user_id.clone())).await?;
+    local_user_db_service
+        .exists(IdentIdName::Id(other_user_id.clone()))
+        .await?;
 
     let comm = get_profile_community(&_db, &ctx, user_id.clone()).await?;
     let discussions = comm.profile_chats.clone().unwrap_or(vec![]);
-    let comm_db_service = CommunityDbService { db: &_db, ctx: &ctx };
-    let discussion_db_service = DiscussionDbService { db: &_db, ctx: &ctx };
-    let existing = discussion_db_service.get_chatroom_with_users(discussions, vec![user_id.clone(), other_user_id.clone()]).await?;
-    let discussion = match existing {
-        None => create_chat_discussion(user_id.clone(), other_user_id, comm, comm_db_service, discussion_db_service).await?,
-        Some(disc) => disc
+    let comm_db_service = CommunityDbService {
+        db: &_db,
+        ctx: &ctx,
     };
-    ctx.to_htmx_or_json_res(ProfileChat { discussion, user_id })
+    let discussion_db_service = DiscussionDbService {
+        db: &_db,
+        ctx: &ctx,
+    };
+    let existing = discussion_db_service
+        .get_chatroom_with_users(discussions, vec![user_id.clone(), other_user_id.clone()])
+        .await?;
+    let discussion = match existing {
+        None => {
+            create_chat_discussion(
+                user_id.clone(),
+                other_user_id,
+                comm,
+                comm_db_service,
+                discussion_db_service,
+            )
+            .await?
+        }
+        Some(disc) => disc,
+    };
+    ctx.to_htmx_or_json_res(ProfileChat {
+        discussion,
+        user_id,
+    })
 }
 
-async fn create_chat_discussion<'a>(user_id: Thing, other_user_id: Thing, comm: Community, comm_db_service: CommunityDbService<'a>, discussion_db_service: DiscussionDbService<'a>) -> CtxResult<Discussion> {
-    let disc = discussion_db_service.create_update(Discussion {
-        id: None,
-        belongs_to: comm.id.unwrap(),
-        title: None,
-        topics: None,
-        chat_room_user_ids: Some(vec![user_id.clone(), other_user_id.clone()]),
-        r_created: None,
-        created_by: user_id.clone(),
-    }).await?;
-    let exists = record_exists(comm_db_service.db, CommunityDbService::get_profile_community_id(user_id.clone())).await;
+async fn create_chat_discussion<'a>(
+    user_id: Thing,
+    other_user_id: Thing,
+    comm: Community,
+    comm_db_service: CommunityDbService<'a>,
+    discussion_db_service: DiscussionDbService<'a>,
+) -> CtxResult<Discussion> {
+    let disc = discussion_db_service
+        .create_update(Discussion {
+            id: None,
+            belongs_to: comm.id.unwrap(),
+            title: None,
+            topics: None,
+            chat_room_user_ids: Some(vec![user_id.clone(), other_user_id.clone()]),
+            r_created: None,
+            created_by: user_id.clone(),
+        })
+        .await?;
+    let exists = record_exists(
+        comm_db_service.db,
+        CommunityDbService::get_profile_community_id(user_id.clone()),
+    )
+    .await;
     if exists.is_err() {
         // creates profile community
         get_profile_community(comm_db_service.db, comm_db_service.ctx, user_id.clone()).await?;
     }
-    let exists = record_exists(comm_db_service.db, CommunityDbService::get_profile_community_id(other_user_id.clone())).await;
+    let exists = record_exists(
+        comm_db_service.db,
+        CommunityDbService::get_profile_community_id(other_user_id.clone()),
+    )
+    .await;
     if exists.is_err() {
         // creates profile community
-        get_profile_community(comm_db_service.db, comm_db_service.ctx, other_user_id.clone()).await?;
+        get_profile_community(
+            comm_db_service.db,
+            comm_db_service.ctx,
+            other_user_id.clone(),
+        )
+        .await?;
     }
 
-    comm_db_service.add_profile_chat_discussion(user_id, disc.id.clone().unwrap()).await?;
-    comm_db_service.add_profile_chat_discussion(other_user_id, disc.id.clone().unwrap()).await?;
+    comm_db_service
+        .add_profile_chat_discussion(user_id, disc.id.clone().unwrap())
+        .await?;
+    comm_db_service
+        .add_profile_chat_discussion(other_user_id, disc.id.clone().unwrap())
+        .await?;
     Ok(disc)
 }
