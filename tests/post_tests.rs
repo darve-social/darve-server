bc86b1c466781d610ed2e6c1a8ffdd435fd3cda7
Maryan <genzhalo@gmail.com>
2025-05-19 18:39:22 +0200

    Merge pull request #27
    * create new folder app structure
    
    * remove workspace folders
    
    * Merge branch 'main' into dir_layout
    
    * add opt-level for release version
    
    * Merge branch 'main' into dir_layout
    
    * use regular fn approach to add_latest_posts on follow route instead s…
    
    * make test for using larger file (7MB) when creating post (#26)
    
    * add new route to search posts by tag - order by time desc
    
    * Merge branch 'main' into get_posts_by_query
    
    * add create_post_with_the_same_name test
    
    * feat: add index for tags
    
    * feat: assert post max tags

--- tests/post_tests.rs
+++ tests/post_tests.rs
@@ -3,23 +3,26 @@ mod helpers;
 use crate::helpers::{create_login_test_user, create_test_server};
 use axum::extract::{Path, State};
 use axum_test::multipart::MultipartForm;
-use community_entity::{Community, CommunityDbService};
-use community_routes::{get_community, CommunityInput};
+use community_entity::CommunityDbService;
+use community_routes::get_community;
 use darve_server::entities::community::community_entity;
+use darve_server::entities::community::post_entity::Post;
 use darve_server::entities::community::post_entity::PostDbService;
 use darve_server::middleware::error::CtxResult;
 use darve_server::middleware::utils::db_utils::RecordWithId;
 use darve_server::middleware::utils::string_utils::get_string_thing;
 use darve_server::middleware::{self, db};
 use darve_server::routes::community::community_routes;
+use darve_server::routes::community::post_routes::GetPostsQuery;
 use darve_server::routes::community::profile_routes::get_profile_community;
 use helpers::community_helpers::create_fake_community;
+use helpers::post_helpers::get_posts;
 use helpers::post_helpers::{
     create_fake_post, create_fake_post_with_file, create_fake_post_with_large_file,
 };
 use middleware::ctx::Ctx;
 use middleware::utils::extractor_utils::DiscussionParams;
-use middleware::utils::request_utils::CreatedResponse;
+use serde_json::{from_value, Value};
 use surrealdb::sql::Thing;
 use uuid::Uuid;
 
@@ -28,89 +31,19 @@ async fn create_post() {
     let (server, ctx_state) = create_test_server().await;
     let (server, user_ident) = create_login_test_user(&server, "usnnnn".to_string()).await;
 
-    let comm_name = "comm-naMMe1".to_lowercase();
-
-    // create community
-    let create_response = server
-        .post("/api/community")
-        .json(&CommunityInput {
-            id: "".to_string(),
-            name_uri: comm_name.clone(),
-            title: "The Community Test".to_string(),
-        })
-        .add_header("Accept", "application/json")
-        .await;
-    let created = &create_response.json::<CreatedResponse>();
-
-    let comm_id = Thing::try_from(created.id.clone()).unwrap();
-    let comm_name = created.uri.clone().unwrap();
-    create_response.assert_status_success();
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
 
     let ctx = Ctx::new(Ok(user_ident), Uuid::new_v4(), false);
-    let community_db_service = CommunityDbService {
-        db: &ctx_state._db,
-        ctx: &ctx,
-    };
-    let community: Community = community_db_service
-        .db
-        .select((&comm_id.tb, comm_id.id.to_raw()))
-        .await
-        .unwrap()
-        .unwrap();
-    assert_eq!(comm_name, community.name_uri.clone());
-    let community_discussion_id = community.profile_discussion.clone().unwrap();
 
-    let post_name = "post title Name 1".to_string();
-    let create_post = server
-        .post(format!("/api/discussion/{community_discussion_id}/post").as_str())
-        .multipart(
-            MultipartForm::new()
-                .add_text("title", post_name.clone())
-                .add_text("content", "contentttt")
-                .add_text("topic_id", ""),
-        )
-        .add_header("Accept", "application/json")
-        .await;
-    let created = create_post.json::<CreatedResponse>();
-    create_post.assert_status_success();
-    assert_eq!(created.id.len() > 0, true);
-
-    let post_name2 = "post title Name 2?&$^%! <>end".to_string();
-    let create_response2 = server
-        .post(format!("/api/discussion/{community_discussion_id}/post").as_str())
-        .multipart(
-            MultipartForm::new()
-                .add_text("title", post_name2.clone())
-                .add_text("content", "contentttt222")
-                .add_text("topic_id", ""),
-        )
-        .add_header("Accept", "application/json")
-        .await;
-
-    let create_response4 = server
-        .post(format!("/api/discussion/{community_discussion_id}/post").as_str())
-        .multipart(
-            MultipartForm::new()
-                .add_text("title", post_name2.clone())
-                .add_text("content", "contentttt444442")
-                .add_text("topic_id", ""),
-        )
-        .add_header("Accept", "application/json")
-        .await;
-
-    create_response2.assert_status_success();
-    // can't have same title
-    create_response4.assert_status_bad_request();
-
-    let _ = server
-        .get(format!("/api/discussion/{community_discussion_id}/post").as_str())
-        .add_header("Accept", "application/json")
-        .await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
 
     let comm_view = get_community(
         State(ctx_state.clone()),
         ctx,
-        Path(comm_name),
+        Path(result.name),
         DiscussionParams {
             topic_id: None,
             start: None,
@@ -125,7 +58,35 @@ async fn create_post() {
         .profile_discussion_view
         .unwrap()
         .posts;
-    assert_eq!(posts.len(), 2);
+    assert_eq!(posts.len(), 4);
+}
+
+#[tokio::test]
+async fn create_post_with_the_same_name() {
+    let (server, ctx_state) = create_test_server().await;
+    let (server, user_ident) = create_login_test_user(&server, "usnnnn".to_string()).await;
+
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
+
+    let title = "TEST";
+    let data = MultipartForm::new()
+        .add_text("title", title)
+        .add_text("content", "content")
+        .add_text("topic_id", "");
+
+    let response =
+        helpers::post_helpers::create_post(server, &result.profile_discussion, data).await;
+
+    response.assert_status_success();
+
+    let data_1 = MultipartForm::new()
+        .add_text("title", title)
+        .add_text("content", "content")
+        .add_text("topic_id", "");
+    let response_1 =
+        helpers::post_helpers::create_post(server, &result.profile_discussion, data_1).await;
+
+    response_1.assert_status_bad_request();
 }
 
 #[tokio::test]
@@ -150,11 +111,11 @@ async fn get_latest() {
         .unwrap()
         .profile_discussion
         .unwrap();
-    let _ = create_fake_post(server, &profile_discussion, None).await;
-    let _ = create_fake_post(server, &profile_discussion, None).await;
-    let _ = create_fake_post(server, &profile_discussion, None).await;
-    let _ = create_fake_post(server, &profile_discussion, None).await;
-    
+    let _ = create_fake_post(server, &profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &profile_discussion, None, None).await;
+    let _ = create_fake_post(server, &profile_discussion, None, None).await;
+
     let profile_comm = CommunityDbService {
         ctx: &ctx,
         db: &ctx_state._db,
@@ -173,6 +134,125 @@ async fn get_latest() {
     let result = get_latest_posts(1, discussion_id, &ctx, &ctx_state._db).await;
     assert!(result.is_ok());
     assert_eq!(result.unwrap().len(), 1)
+}
+
+#[tokio::test]
+async fn create_post_with_tags() {
+    let (server, ctx_state) = create_test_server().await;
+    let (server, user_ident) = create_login_test_user(&server, "usnnnn".to_string()).await;
+
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
+    let tags = vec!["tag".to_string(), "tag1".to_string(), "tag2".to_string(), "tag3".to_string(), "tag4".to_string()];
+    let _ = create_fake_post(server, &result.profile_discussion, None, Some(tags.clone())).await;
+    let posts_res = get_posts(&server, None).await;
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    let max_tags = 5;
+    let tags_to_save = tags.len();
+    assert_eq!(max_tags == tags_to_save, true);
+    assert_eq!(posts.len(), 2);
+    assert_eq!(posts[0].tags.as_ref().unwrap().len(), max_tags);
+    assert_eq!(posts[0].tags.as_ref().unwrap()[0], tags[0]);
+    assert_eq!(posts[0].tags.as_ref().unwrap()[1], tags[1]);
+    assert_eq!(posts[1].tags, None);
+    // TODO -test gt max fail- test it fails with more than max tags
+}
+
+#[tokio::test]
+async fn filter_posts_by_tag() {
+    let (server, ctx_state) = create_test_server().await;
+    let (server, user_ident) = create_login_test_user(&server, "usnnnn".to_string()).await;
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
+    let tags = vec!["tag".to_string(), "tag1".to_string()];
+
+    let _ = create_fake_post(server, &result.profile_discussion, None, None).await;
+    let _ = create_fake_post(
+        server,
+        &result.profile_discussion,
+        None,
+        Some(vec![tags[0].clone()]),
+    )
+    .await;
+    let _ = create_fake_post(server, &result.profile_discussion, None, Some(tags.clone())).await;
+    let _ = create_fake_post(
+        server,
+        &result.profile_discussion,
+        None,
+        Some(vec!["non_of_them".to_string()]),
+    )
+    .await;
+
+    let posts_res = get_posts(&server, None).await;
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    assert_eq!(posts.len(), 4);
+
+    let posts_res = get_posts(
+        &server,
+        Some(GetPostsQuery {
+            tag: Some(tags[0].clone()),
+            start: None,
+            count: None,
+            order_dir: None,
+        }),
+    )
+    .await;
+
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    assert_eq!(posts.len(), 2);
+
+    let posts_res = get_posts(
+        &server,
+        Some(GetPostsQuery {
+            tag: Some(tags[1].clone()),
+            start: None,
+            count: None,
+            order_dir: None,
+        }),
+    )
+    .await;
+
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    assert_eq!(posts.len(), 1);
+
+    let posts_res = get_posts(
+        &server,
+        Some(GetPostsQuery {
+            tag: Some("rust".to_string()),
+            start: None,
+            count: None,
+            order_dir: None,
+        }),
+    )
+    .await;
+
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    assert_eq!(posts.len(), 0);
+
+    let posts_res = get_posts(
+        &server,
+        Some(GetPostsQuery {
+            tag: None,
+            start: None,
+            count: Some(1),
+            order_dir: None,
+        }),
+    )
+    .await;
+
+    posts_res.assert_status_success();
+    let posts_value = posts_res.json::<Value>();
+    let posts: Vec<Post> = from_value(posts_value.get("posts").unwrap().to_owned()).unwrap();
+    assert_eq!(posts.len(), 1);
 }
 
 async fn get_latest_posts(
