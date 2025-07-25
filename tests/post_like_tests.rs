ba3f09d20ae3a20d37d449b621f96267136f4293
Maryan <genzhalo@gmail.com>
2025-07-25 18:03:27 +0200

    likes with quantity/power (#93)
    * likes with quantity/power
    
    https://app.clickup.com/t/86c4fzvjk
    
    * improve select query
    
    * chore: like count to u16
    
    * feat: change value on already liked
    
    * // TODO -optimize- don't check just update in query if exists
    
    * remove print
    
    ---------
    
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- tests/post_like_tests.rs
+++ tests/post_like_tests.rs
@@ -13,14 +13,10 @@ test_with_server!(create_post_like, |server, ctx_state, config| {
     let result = create_fake_post(server, &result.default_discussion, None, None).await;
 
     // check like and number
-    let response = post_helpers::create_post_like(&server, &result.id).await;
+    let response = post_helpers::create_post_like(&server, &result.id, None).await;
     response.assert_status_ok();
     let likes_nr = response.json::<PostLikeResponse>().likes_count;
     assert_eq!(likes_nr, 1);
-
-    // throw if like same post again
-    let response = post_helpers::create_post_like(&server, &result.id).await;
-    response.assert_status_failure();
 
     // check delete and count
     let response = post_helpers::delete_post_like(&server, &result.id).await;
@@ -29,6 +25,37 @@ test_with_server!(create_post_like, |server, ctx_state, config| {
     assert_eq!(likes_nr, 0);
 
     // throw if like post that does not exist
-    let response = post_helpers::create_post_like(&server, "post:that_does_not_exist").await;
+    let response = post_helpers::create_post_like(&server, "post:that_does_not_exist", None).await;
     response.assert_status_failure();
+});
+
+test_with_server!(create_post_like_with_count, |server, ctx_state, config| {
+    let (server, user, _, _) = create_fake_login_test_user(&server).await;
+    let user_ident = user.id.as_ref().unwrap().to_raw();
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
+
+    let result = create_fake_post(server, &result.default_discussion, None, None).await;
+
+    // check like and number
+    let response = post_helpers::create_post_like(&server, &result.id, Some(4)).await;
+    response.assert_status_ok();
+    let likes_nr = response.json::<PostLikeResponse>().likes_count;
+    assert_eq!(likes_nr, 4);
+});
+
+test_with_server!(udpate_likes, |server, ctx_state, config| {
+    let (server, user, _, _) = create_fake_login_test_user(&server).await;
+    let user_ident = user.id.as_ref().unwrap().to_raw();
+    let result = create_fake_community(server, &ctx_state, user_ident.clone()).await;
+    let result = create_fake_post(server, &result.default_discussion, None, None).await;
+
+    let response = post_helpers::create_post_like(&server, &result.id, None).await;
+    response.assert_status_ok();
+    let likes_nr = response.json::<PostLikeResponse>().likes_count;
+    assert_eq!(likes_nr, 1);
+
+    let response = post_helpers::create_post_like(&server, &result.id, Some(10)).await;
+    response.assert_status_ok();
+    let likes_nr = response.json::<PostLikeResponse>().likes_count;
+    assert_eq!(likes_nr, 10);
 });
