cdcfca50b0e93388f025075fc1c6cdb220b65d42
matjazonline <matjazonline@gmail.com>
2025-09-05 23:34:36 +0200

    fix: stripe amt received

--- src/routes/webhooks/stripe.rs
+++ src/routes/webhooks/stripe.rs
@@ -56,8 +56,7 @@ async fn handle_webhook(
 
     match payment_intent {
         Some(payment_intent) => {
-            // TODO -fixed_decimals- amount in db should be in fixed with decimals
-            let amount_received = payment_intent.amount_received / 100;
+            let amount_received = payment_intent.amount_received;
             if amount_received <= 0 {
                 return Ok("No amount received".into_response());
             }
