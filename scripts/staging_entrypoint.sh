b04d481651c61f61f9844fd0059e8bfe59f4b0b8
Marjan Genzhalo <genzhalo@gmail.com>
2025-10-06 14:47:40 +0200

    remove redundat files

--- scripts/staging_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Staging environment detected: inserting test users..."
-SCHEMA="http" HOST="localhost" PORT="8080" API_PATH="/api/register" ./init_test_data.sh &
-
-exec /usr/local/bin/app
